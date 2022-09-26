c9fcf1e52896f5bf661524fc442db962e2d38403
Syndafloden <coloss345@gmail.com>
2022-09-26 19:24:02 +0700

    Add application class

---
+++ CgEngineCore/includes/CgEngineCore/Application.hpp
@@ -0,0 +1,19 @@
+
+namespace CGEngine {
+
+	class Application
+	{
+	public:
+
+		Application();
+		virtual ~Application();
+
+		Application(const Application&) = delete;
+		Application(Application&&) = delete;
+		Application& operator = (const Application&) = delete;
+
+		virtual int start(unsigned int window_width, unsigned int window_height, const char* title);
+
+		virtual void on_update(){}
+	};
+}
--- CgEngineEditor/src/main.cpp
+++ CgEngineEditor/src/main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
-#include <CgEngineCore/test.hpp>
+#include<memory>
+#include"CgEngineCore/Application.hpp"
+
+class MyApp : public CGEngine::Application {
+	virtual void on_update() override {
+		std::cout << "Update: " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+ };
 
 int main() {
 
-	CGEngine::checkglfw();
+	auto myApp = std::make_unique<MyApp>();
+
+	int returnCode = myApp->start(1024, 768, "Myapp");
 
 	return 0;
 }
