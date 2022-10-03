dc5b608ddd305c18185799fd1fc80e8ed83034ec
Syndafloden <coloss345@gmail.com>
2022-10-03 15:47:33 +0700

    Create shader module

--- CgEngineCore/src/CgEngineCore/Window.cpp
+++ CgEngineCore/src/CgEngineCore/Window.cpp
@@ -1,5 +1,6 @@
 #include "CgEngineCore/Window.hpp"
 #include "CgEngineCore/Log.hpp"
+#include "CgEngineCore/Rendering/ShaderProgram.hpp"
 
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
@@ -9,6 +10,38 @@
 #include<imgui/backends/imgui_impl_glfw.h>
 namespace CGEngine {
     static bool s_GLFW_initialized = false;
+
+    GLfloat points[] = {
+        0.0f,0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        -0.5f,-0.5f,0.0f
+    };
+
+    GLfloat colors[] = {
+        1.0f,0.0f,0.0f,
+        0.0f,1.f,0.f,
+        0.f,0.f,1.f
+    };
+
+    const char* vertex_shader =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;\n"
+        "layout(location = 1) in vec3 vertex_color;\n"
+        "out vec3 color;\n"
+        "void main() {\n"
+        "	color = vertex_color;\n"
+        "	gl_Position = vec4(vertex_position, 1.0);\n"
+        " }";
+    const char* fragment_shader =
+        "#version 460\n"
+		"in vec3 color;\n"
+		"out vec4 frag_color;\n"
+		"void main() {\n"
+		"	frag_color = vec4(color,1.0); \n"
+		" }";
+
+    std::unique_ptr<ShaderProgram> p_shader_program;
+    GLuint vao;
 	
     Window::Window(std::string title, const unsigned int width, const unsigned int height) :m_data({ std::move(title) ,width, height })
 	{
@@ -29,11 +62,17 @@ namespace CGEngine {
 
 	void Window::on_update()
 	{
+        
         glClearColor(m_background_color[0], m_background_color[1], m_background_color[2], m_background_color[3]);
        
-        
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        p_shader_program->bind();
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+       
+
 
         ImGuiIO& io = ImGui::GetIO();
 
@@ -133,7 +172,42 @@ namespace CGEngine {
 
                 data.eventCallbackFn(event);
             });
+        glfwSetFramebufferSizeCallback(m_pWindow,
+            [](GLFWwindow* pWindow, int width, int height)
+            {
+
+                glViewport(0, 0, width, height);
+            }
+        );
+       
         
+        p_shader_program = std::make_unique<ShaderProgram>(vertex_shader, fragment_shader);
+        if (!p_shader_program->isCompiled()) {
+            return false;
+        }
+        
+     
+
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         
         return 0;
